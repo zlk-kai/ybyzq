3b198772cc6e1b59b015325737fb00e7045c39e7
zlk-kai <zlk990414@163.com>
2022-03-23 23:21:38 +0800

    搭建springboot后台

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/zh-CN'
 import store from './store'
+import "@/assets/css/global.css"
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI,{locale,size:"mini"});
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    el: '#app',
+    render: h => h(App)
 }).$mount('#app')
